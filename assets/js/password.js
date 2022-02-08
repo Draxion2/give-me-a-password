6ee14eadc7e7b0de15d35d3b28e64fa102a74376
= <holdanmcmullen@gmail.com>
2022-02-07 18:59:28 -0700

    pseudocode early html/css

---
+++ assets/js/password.js
@@ -0,0 +1,11 @@
+
+
+// Click a 'button' and generates a password //
+
+// ask user to or NOT include lowercase, uppercase, numeric, and/or special characters //
+
+// input should be validated and at least one character type should be selected //
+
+// After all prompts are answered by the user then a password will generate that matches //
+
+// password then is displayed as an alert or on the webpage //
