6099ed80fdcf4ffd7b9078f3e289805b38739b8b
= <holdanmcmullen@gmail.com>
2022-02-08 00:32:51 -0700

    reverted code

--- assets/js/password.js
+++ assets/js/password.js
@@ -9,23 +9,3 @@
 // After all prompts are answered by the user then a password will generate that matches //
 
 // password then is displayed as an alert or on the webpage //
-
-/*
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
